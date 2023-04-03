620e6197f62026995bf1d89159fa6909ea95a3c7
xbprm <62511682+xbprm@users.noreply.github.com>
2023-04-03 13:16:32 +0000

    - resolve Pylint issues

--- ResourceMonitor/main.py
+++ ResourceMonitor/main.py
@@ -3,18 +3,19 @@ import psutil
 
 
 def display_usage(cpu_usage, memory_usage, bars=50):
-    cpu_percent = (cpu_usage / 100.0)
+    cpu_percent = cpu_usage / 100.0
     cpu_bar = "█" * int(cpu_percent * bars) + "-" * (bars - int(cpu_percent * bars))
-    memory_percent = (memory_usage / 100.0)
+    memory_percent = memory_usage / 100.0
     memory_bar = "█" * int(memory_percent * bars) + "-" * (bars - int(memory_percent * bars))
-    
+
     print(f"\rCPU Usage: |{cpu_bar}| {cpu_usage:.2f}%  ", end="")
     print(f"MEMORY Usage: |{memory_bar}| {memory_usage:.2f}%  ", end="\r")
 
-def main():    
+def main():
     while True:
         display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
         time.sleep(0.5)
 
 if __name__ == "__main__":
     main()
+    
