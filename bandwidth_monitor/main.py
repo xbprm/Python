7caeb62623f214ddf883913eb1ec074bb4c59d9c
Börge Rothfuß <boerge_rothfuss@web.de>
2023-07-22 18:29:52 +0200

    Revert "- optimize and comment code"
    This reverts commit af8728f4ed0a84b9638b99b865b3ba1315ab30b0.

--- bandwidth_monitor/main.py
+++ bandwidth_monitor/main.py
@@ -1,61 +1,36 @@
-# Import the required libraries
-import psutil  # for getting network IO statistics
-import time  # for sleeping for a specified duration
+import psutil  # Import the psutil library
+import time  # Import the time library
 
-# Define the get_net_io_stats function
-def get_net_io_stats():
-    # Use the psutil library to get the current network IO statistics
-    return psutil.net_io_counters()
-
-# Define the print_net_io_stats function
-def print_net_io_stats(last_stats):
-    # Use the get_net_io_stats function to get the current network IO statistics
-    current_stats = get_net_io_stats()
-
-    # Calculate the new network IO statistics
-    new_received = current_stats.bytes_recv - last_stats.bytes_recv
-    new_sent = current_stats.bytes_sent - last_stats.bytes_sent
-    new_total = new_received + new_sent
-
-    # Convert the new network IO statistics to MB/s
-    mb_new_received = new_received / 1024 / 1024
-    mb_new_sent = new_sent / 1024 / 1024
-    mb_new_total = new_total / 1024 / 1024
-
-    # Print the new network IO statistics
-    print(f"Received: {mb_new_received:.2f} MB/s, Sent: {mb_new_sent:.2f} MB/s, Total: {mb_new_total:.2f} MB/s")
-
-    # Update the last network IO statistics
-    last_stats = current_stats
-
-# Define the main function
 if __name__ == '__main__':
     # Get the initial network IO statistics
-    last_stats = get_net_io_stats()
-
-    # Start an infinite loop
-    while True:
-        # Use the print_net_io_stats function to print the new network IO statistics
-        print_net_io_stats(last_stats)
-
-        # Sleep for 1 second
-        time.sleep(1)
-
-# Define the main function again (this time with an additional while loop)
-if __name__ == '__main__':
-    # Get the initial network IO statistics
-    last_stats = get_net_io_stats()
+    last_received = psutil.net_io_counters().bytes_recv
+    last_sent = psutil.net_io_counters().bytes_sent
+    last_total = last_received + last_sent
 
     # Start an infinite loop
     while True:
         # Get the current network IO statistics
-        current_stats = get_net_io_stats()
+        bytes_received = psutil.net_io_counters().bytes_recv
+        bytes_sent = psutil.net_io_counters().bytes_sent
+        bytes_total = bytes_received + bytes_sent
 
-        # Use the print_net_io_stats function to print the new network IO statistics
-        print_net_io_stats(last_stats)
+        # Calculate the new network IO statistics
+        new_received = bytes_received - last_received
+        new_sent = bytes_sent - last_sent
+        new_total = bytes_total - last_total
+
+        # Convert the new network IO statistics to MB/s
+        mb_new_received = new_received / 1024 / 1024
+        mb_new_sent = new_sent / 1024 / 1024
+        mb_new_total = new_total / 1024 / 1024
+
+        # Print the new network IO statistics
+        print(f"Received: {mb_new_received:.2f} MB/s, Sent: {mb_new_sent:.2f} MB/s, Total: {mb_new_total:.2f} MB/s")
 
         # Update the last network IO statistics
-        last_stats = current_stats
+        last_received = bytes_received
+        last_sent = bytes_sent
+        last_total = bytes_total
 
         # Sleep for 1 second
         time.sleep(1)
