410e9d0010c1b588d3618843803ceb5b1884941e
Börge Rothfuß <boerge_rothfuss@web.de>
2024-07-26 18:50:11 +0200

    - accept cookies

--- check_attendance/main.py
+++ check_attendance/main.py
@@ -1,12 +1,16 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Set the path to the ChromeDriver executable
 chrome_driver_path = r"C:\Util\chromedriver-win64"
 
-# define ChromeOptions
+# Define ChromeOptions
 options = webdriver.ChromeOptions()
 
-# add the option for the executable path to ChromeDriver
+# Add the option for the executable path to ChromeDriver
 options.add_argument(f"--executable-path={chrome_driver_path}")
 
 # Create a ChromeDriver instance
@@ -21,13 +25,30 @@ driver.get(url)
 # Wait for the page to load completely (optional)
 driver.implicitly_wait(10)  # Wait for 10 seconds
 
+# Wait for the cookie consent button to appear and click it
+cookie_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[@class='cookie-consent__button']"))
+)
+cookie_button.click()
+
+# Wait for the page to load after accepting cookies
+WebDriverWait(driver, 10).until(EC.staleness_of(cookie_button))
+
+# Get the absolute path of the script file
+script_path = os.path.abspath(__file__)
+
+# Get the directory path of the script file
+script_directory = os.path.dirname(script_path)
+
+# Define the path to save the downloaded page
+downloaded_page_path = os.path.join(script_directory, "downloaded_website.html")
+
 # Save the page source to a file
-with open("downloaded_website.html", "w", encoding="utf-8") as file:
+with open(downloaded_page_path, "w", encoding="utf-8") as file:
     file.write(driver.page_source)
 
 # Close the ChromeDriver instance
 driver.quit()
 
-# define string to search for
+# Define string to search for
 string_to_search = r"Herren 40 Einzel"
-
